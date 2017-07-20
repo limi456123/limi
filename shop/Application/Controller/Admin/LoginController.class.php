5c694a8e29ef0438e3bccda5a8e6c917d1d6925f
123 <123@qq.com>
2017-07-20 17:34:58 +0800

    后台首页显示

--- shop/Application/Controller/Admin/LoginController.class.php
+++ shop/Application/Controller/Admin/LoginController.class.php
@@ -1,14 +1,34 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2017/7/20
- * Time: 9:08
- */
-class LoginController
+
+class LoginController extends Controller
 {
-	public function a(){
-		SearchPageTool::SearchPage('page','x');
-	}
+    public function Login(){
+        $this->display("login");
+    }
+    public function check(){
+        $data=$_POST;
+        $captcha = $_POST['captcha'];
+        session_start();
+       if(strtoupper($captcha)!=strtoupper($_SESSION['random'])){
+               $this->redirect("index.php?p=Admin&c=login&a=login","验证码错误",3);
+       }
+              $categoryModel =new categoryModel();
+              $result = $categoryModel->check($data);
+       if( $result!==false) {
+                //$this->redirect("index.php?p=Admin&c=login&a=login");
+                $_SESSION["user"] = $result;
+                if (isset($data['remember'])) {
+                    setcookie("id", $result['id'], time() + 24 * 60 * 60, "/");
+                    setcookie("password", $result['password'], time() + 24 * 60 * 60, "/");
+
+                }
+                $this->redirect("index.php?p=Admin&c=Index&a=index","正在登录后台",3);
+       }
+
+
+        $this->redirect("index.php?p=Admin&c=login&a=login","用户名或密码",3);
+    }
+
+
 }
